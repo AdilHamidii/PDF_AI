19093d471b5e08c126255a802d29b455c7e60e98
Adilhamidi <adil.hamidii123@gmail.com>
2026-03-10 07:33:17 +0100

    Initial Commit

---
+++ API AI app/API_AI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_AI_appApp.swift
+//  API AI app
+//
+//  Created by Adyl on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct API_AI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
